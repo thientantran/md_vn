a03582d8b25bd1e9482fb970503ff0367c515016
thientantran <thientantm@gmail.com>
2023-12-13 15:33:48 +0700

    Create app
    npx create-next-app@latest my-app
    npx shadcn-ui@latest init
    add button component
    fix page.jsx and layout.jsx
    add html and body into global css

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <h className="text-blue-500">Hello world</h>
+      <Button variant='destructive'>
+        hellow
+      </Button>
+    </div>
+  )
+}
