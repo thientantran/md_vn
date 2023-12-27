5bf2f4e952a3d91eab84fd40a325efcf36042b99
thientantran <thientantm@gmail.com>
2023-12-27 15:30:07 +0700

    Post - TABLE - ALL - Sorting, Actions, Searching....
    fixup

--- app/(dashboard)/writer/posts/page.jsx
+++ app/(dashboard)/writer/posts/page.jsx
@@ -1,6 +1,23 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { DataTable } from "@/components/DataTable";
+import { columns } from "@/components/writer/Columns";
+import prismadb from "@/lib/prisma";
+import { getServerSession } from "next-auth";
 
-export default function page() {
+export default async function page() {
+  const session = getServerSession(authOptions)
+  if(!session){
+    return redirect("/")
+  }
+  const posts = await prismadb.post.findMany({
+    orderBy: {
+      createdAt:'desc'
+    },
+  })
   return (
-    <div>page</div>
+    <div className="p-6">
+
+      <DataTable columns={columns} data={posts} />
+    </div>
   )
 }
