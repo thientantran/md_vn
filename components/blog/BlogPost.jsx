4cc2b3fb0c8b18ed408acf3e10347f854ba53809
Tan Tran <thientantm@gmail.com>
2023-12-27 21:45:22 +0700

    POST - Get Text from html, binding and limit 2 rows

--- components/blog/BlogPost.jsx
+++ components/blog/BlogPost.jsx
@@ -1,7 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
+function extractTextFromHTML(htmlString) {
+  const dom = new JSDOM(htmlString);
+  return dom.window.document.body.textContent || "";
+}
 export default function BlogPost({post}) {
   const date = new Date(post.createdAt);
 
@@ -11,7 +17,7 @@ export default function BlogPost({post}) {
     month: 'long',
     day: 'numeric',
   });
-  console.log(post)
+  const textContent = extractTextFromHTML(post.desc).substring(0, 1000);
   return (
       <div className="px-2 sm:px-4 md:px-6 lg:px=8 my-1 md:my-4 py-3 sm:py-6 bg-white rounded-lg shadow-md">
         <div className="flex justify-between items-center">
@@ -24,7 +30,7 @@ export default function BlogPost({post}) {
           </div>
           <div className="sm:flex-[5]  md:flex-[5] lg:flex-[4]">
             <Link className="text-base md:text-xl font-semibold text-gray-700 hover:text-gray-600" href="/blog">{post.title}</Link>
-            <p className="mt-2 text-xs md:text-sm text-gray-600">Sabine Roman, MD, PhD, associate professor of gastroenterology and physiology at Lyon University Hospital in France, took the floor at the United European Gastroenterology Week to discuss the link between a chronic cough and gastroesophageal reflux disease (GERD)...</p>
+            <p className="mt-2 text-xs md:text-sm text-gray-600 line-clamp-3">{textContent}</p>
           </div>
         </div>
         <div className="flex justify-between items-center mt-4">
